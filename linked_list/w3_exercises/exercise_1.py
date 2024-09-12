af84e29082d666223b68bc5cf2a2529d862f46d3
atunnd <atuan.ngndai@gmail.com>
2024-09-12 16:57:02 +0700

    linked_list/edit exercise1 (function create_linked_list change code to link new node) and adding exercise 2

--- linked_list/w3_exercises/exercise_1.py
+++ linked_list/w3_exercises/exercise_1.py
@@ -2,37 +2,43 @@
 Write a Python program to create a singly linked list, append some items and iterate through the list.
 '''
 
+
 class Node:
     def __init__(self, data=None, next=None):
         self._data = data
         self._next = next
-    
+
+    def __str__(self):
+        return f"Data: {self._data} - Next: {self._next}"
+
     def get_data(self):
         return self._data
-    
+
     def set_data(self, data):
         self._data = data
-    
+
     def del_data(self):
         del self._data
 
     def get_next(self):
         return self._next
-    
+
     def set_next(self, next):
         self._next = next
-    
+
     def del_next(self):
         del self._next
 
     data = property(get_data, set_data, del_data)
     next = property(get_next, set_next, del_next)
 
+
 class LinkedList:
     def __init__(self):
         self._head = None
         self._tail = None
-    
+
+    ''' # Get bug for this block of code? Still dont know why, but i guess it's related to the property
     def get_head(self):
         return self._head
     
@@ -53,39 +59,35 @@ class LinkedList:
 
     head = property(get_head, set_head, del_head)
     tail = property(get_tail, set_tail, del_tail)
-
-def init_linked_list(ll):
-    ll.head = Node()
-    ll.tail = Node()
-
-def create_node(data):
-    new_node = Node(data)
-    return new_node
-
-def create_linked_list(data):
-    ll = LinkedList()
-    init_linked_list(ll)
-    ll.head.data = data[0]
-    moving_head = ll.head
+    '''
+    def create_linked_list_from_array(self, data):
+        for i, value in enumerate(data):
+            new_node = Node(value)
+            if i == 0:
+                self._head = new_node
+                self._tail = new_node
+            else:
+                self._tail.next = new_node
+                self._tail = new_node
     
-    for i, value in enumerate(data):
-        if i == 0:
-            continue
-        new_node = create_node(value)
-        moving_head.next = new_node
-        moving_head = new_node
-        if i == len(data)-1:
-            ll.tail = moving_head
-    return ll
+    def add_node(self, data):
+        new_node = Node(data)
+        if self._head == None:
+            self._head = new_node
+            self._tail = new_node
+        else:
+            self._tail.next = new_node
+            self._tail = new_node
 
-def print_linked_list(head):
-    while(head != None):
-        print(f"[{head.data}]")
-        head = head.next
+    def print_linked_list(self):
+        temp = self._head
+        while (temp != None):
+            print(f"[{temp.data}]")
+            temp = temp.next
+    
 
 if __name__ == '__main__':
-    ll = create_linked_list([1, 2, 3, 4])
-    print_linked_list(ll.head)
-
-    ll2 = create_linked_list([4, 3, 2, 1])
-    print_linked_list(ll2.head)
+    ll = LinkedList()
+    ll.create_linked_list_from_array([1,5,2,3,45])
+    ll.add_node(90)
+    ll.print_linked_list()
